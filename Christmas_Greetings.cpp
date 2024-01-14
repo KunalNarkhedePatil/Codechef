761a68e755c88b54e1860f989372125928fc16f8
KunalNarkhedePatil <narkhedekunal53@gmail.com>
2024-01-14 12:24:44 +0530

    commit

---
+++ Christmas_Greetings.cpp
@@ -0,0 +1,17 @@
+//Link:-https://www.codechef.com/problems/CHRISTGREET
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int day=0;
+	cin>>day;
+	if(day==25)
+	{
+	    cout<<"CHRISTMAS"<<endl;
+	}
+	else
+	{
+	    cout<<"ORDINARY"<<endl;
+	}
+
+}
---
+++ Lucky_Seven.cpp
@@ -0,0 +1,19 @@
+//Link:-https://www.codechef.com/problems/LUCKYSEVEN?tab=ide
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+      
+    char str[10];
+    
+    cin.getline(str,10);
+    
+	for(int i=0;i<10;i++)
+	{
+	    if(i==6)
+	    {
+	        cout<<str[i]<<endl;
+	    }
+	}
+	
+}
