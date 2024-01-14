8ca14207463415f04fc2800849914128f2b26aaf
KunalNarkhedePatil <narkhedekunal53@gmail.com>
2024-01-14 12:32:31 +0530

    cpmiit

---
+++ Clear_Day.cpp
@@ -0,0 +1,12 @@
+//Link:-https://www.codechef.com/problems/CLEARDAY
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+   int X=0,Y=0;
+   
+   cin>>X>>Y;
+   
+   cout<<7-(X+Y)<<endl;
+
+}
---
+++ Codechef_Round.cpp
@@ -0,0 +1,19 @@
+//Link:-https://www.codechef.com/problems/CODECHEF
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int N=0;
+	
+	cin>>N;
+	
+	if(N==4)
+	{
+	    cout<<"Yes"<<endl;
+	}
+	else
+	{
+	    cout<<"No"<<endl;
+	}
+
+}
