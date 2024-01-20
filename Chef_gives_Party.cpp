907ef94060f03c542f962892ea13d9006c94321c
KunalNarkhedePatil <narkhedekunal53@gmail.com>
2024-01-20 23:22:03 +0530

    ccc

--- Chef_gives_Party.cpp
+++ Chef_gives_Party.cpp
@@ -1,24 +1,24 @@
-//Link:-https://www.codechef.com/problems/PARTY2
+// Link:-https://www.codechef.com/problems/PARTY2
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-	int t=0;
-	int N=0,X=0,K=0;
-	cin>>t;
-	
-	while(t--)
-	{
-	    cin>>N>>X>>K;
-	    
-	    if(N*X<=K)
-	    {
-	        cout<<"Yes"<<endl;
-	    }
-	    else
-	    {
-	        cout<<"No"<<endl;
-	    }
-	}
+int main()
+{
+	int t = 0;
+	int N = 0, X = 0, K = 0;
+	cin >> t;
 
+	while (t--)
+	{
+		cin >> N >> X >> K;
+
+		if (N * X <= K)
+		{
+			cout << "Yes" << endl;
+		}
+		else
+		{
+			cout << "No" << endl;
+		}
+	}
 }
