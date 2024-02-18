acf74e8c4d59110e1c3fbb9f01732d9e9ad6dead
KunalNarkhedePatil <narkhedekunal53@gmail.com>
2024-02-18 11:16:07 +0530

    commit

---
+++ Leg_Space.cpp
@@ -0,0 +1,19 @@
+//Link:-https://www.codechef.com/problems/LEGSP
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int N=0,M=0;
+	
+	cin>>N>>M;
+	
+	if(N<M)
+	{
+	    cout<<"YES"<<endl;
+	}
+	else
+	{
+	    cout<<"NO"<<endl;
+	}
+
+}
---
+++ Valentines_Contest.cpp
@@ -0,0 +1,19 @@
+//Link:-https://www.codechef.com/problems/VAL114
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int N=0;
+	
+	cin>>N;
+	
+	if(N==121)
+	{
+	    cout<<"Likely"<<endl;
+	}
+	else
+	{
+	    cout<<"Unlikely"<<endl;
+	}
+
+}
